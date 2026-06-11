19fb260c3b3d4f505b087b5391205eb5f4d51c48
anandjbangad <anandjbangad@gmail.com>
2026-06-11 15:08:19 -0500

    Add amount and currencyCode to home page response

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',
-    braintreeKey: process.env.AUTHORIZATION_KEY
+    braintreeKey: process.env.AUTHORIZATION_KEY,
+    amount: process.env.AMOUNT,
+    currencyCode: process.env.CURRENCY_CODE      
    });
 });
 
